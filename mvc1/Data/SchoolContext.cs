9aa97bcddaa47ea909c9ddaf96d2b0fe97f5f895
Abdellah <abdallahkaba98@gmail.com>
2024-08-23 21:11:01 +0000

    filtrage-recherche-pagination

---
+++ mvc1/Data/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc1.Models;
+
+namespace mvc1.Data
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
