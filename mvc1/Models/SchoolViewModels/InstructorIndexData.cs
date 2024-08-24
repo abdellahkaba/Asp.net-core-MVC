1137bef7c4b2b6805f912d727d0c2e5792819861
Abdellah <abdallahkaba98@gmail.com>
2024-08-24 19:55:07 +0000

    lire-des-donnees-associees

---
+++ mvc1/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+﻿using mvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc1.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
