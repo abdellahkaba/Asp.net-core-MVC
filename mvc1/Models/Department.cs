18c67db93f93377b681a897a63f80b19d0b2561c
Abdellah <abdallahkaba98@gmail.com>
2024-08-25 13:32:19 +0000

    gestion-conflit

--- mvc1/Models/Department.cs
+++ mvc1/Models/Department.cs
@@ -21,6 +21,9 @@ namespace mvc1.Models
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
